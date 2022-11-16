47f837a468dcc730a518f88d539c9b075db76c97
ianfshirley <ianfshirley88@gmail.com>
2022-11-15 17:58:16 -0800

    repo setup complete

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+console.log('our first server');
+
+// REQUIRE
+// in our servers, we have to use 'require' instead of 'import'
+// Here we will list the requirements for a server
+
+// USE
+// once we have required something, we have to use it
+// here is where we will assign the required file a variable
+// React does this in one step with 'import', express takes 2 steps: require & use
+
+// ROUTES
+// this is where we will write handlers for our endpoints
+
+// ERRORS
+// handle any errors
+
+// LISTEN
+// start the server
+
+
